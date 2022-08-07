227bc7cc46dbde8ed27e34c7d15a79a33c39a8ec
taejong <yut951121@gmail.com>
2022-08-07 17:50:26 +0900

    refactor: :recycle: migrate to functional widget

--- lib/views/components/common/common_button.dart
+++ lib/views/components/common/common_button.dart
@@ -1,126 +1,82 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:parking_people_flutter/constants/custom_colors.dart';
 import 'package:parking_people_flutter/utils/colors.dart';
 
-class CommonButton extends StatelessWidget {
-  const CommonButton({
-    super.key,
-    required this.onTap,
-    this.onLongPress,
-    this.text,
-    this.child,
-    this.buttonColor = Colors.white,
-    this.buttonTextColor = Colors.black,
-    this.buttonWidth,
-    this.buttonHeight = 72,
-    this.buttonFontSize = 16.0,
-    this.mini = false,
-    this.forAction = false,
-    this.buttonBorder,
-    this.buttonBorderRadius = 10.0,
-    this.borderWidth = 0,
-    this.bold = false,
-    this.padding,
-  }) : assert(
-          text != null || child != null,
-          'Either text or child content must be given.',
-        );
+part 'common_button.g.dart';
 
-  ///
-  final String? text;
+Color _splashBaseColor(Color buttonColor) =>
+    isDark(buttonColor) ? CustomColors.white : CustomColors.blue;
 
-  ///
-  final Widget? child;
-
-  ///
-  final Color buttonColor;
-
-  ///
-  final Color buttonTextColor;
-
-  ///
-  final double? buttonWidth;
-
-  ///
-  final double buttonHeight;
-
-  /// onTap에 아무것도 없으면 utils.dart에 있는 noop을 넣어줄 것
-  final VoidCallback onTap;
-
-  final VoidCallback? onLongPress;
-
-  final double buttonFontSize;
-
-  final bool mini;
-
-  final bool forAction;
-
-  final Border? buttonBorder;
-
-  final double buttonBorderRadius;
-
-  final double borderWidth;
-
-  final bool bold;
-
-  final EdgeInsets? padding;
-
-  Color get _splashBaseColor =>
-      isDark(buttonColor) ? CustomColors.white : CustomColors.blue;
-
-  @override
-  Widget build(final BuildContext context) => Container(
-        clipBehavior: Clip.antiAlias,
-        width: mini
-            ? null
-            : (buttonWidth ?? (forAction ? buttonHeight : double.infinity)),
-        height: mini ? null : buttonHeight,
-        padding: mini ? const EdgeInsets.all(8) : null,
-        decoration: BoxDecoration(
-          border: buttonBorder ??
-              (borderWidth > 0
-                  ? Border.all(
-                      color: Color.lerp(buttonColor, CustomColors.black, .06)!,
-                      width: borderWidth,
-                    )
-                  : null),
-          borderRadius: BorderRadius.circular(
-            forAction ? buttonHeight : buttonBorderRadius,
-          ),
-          color: buttonColor,
+@swidget
+Widget commonButton({
+  required VoidCallback onTap,
+  VoidCallback? onLongPress,
+  String? text,
+  Widget? child,
+  Color buttonColor = Colors.white,
+  Color buttonTextColor = Colors.black,
+  double? buttonWidth,
+  double buttonHeight = 72,
+  double buttonFontSize = 16,
+  bool mini = false,
+  bool forAction = false,
+  Border? buttonBorder,
+  double buttonBorderRadius = 10,
+  double borderWidth = 0,
+  bool bold = false,
+  EdgeInsets? padding,
+}) =>
+    Container(
+      clipBehavior: Clip.antiAlias,
+      width: mini
+          ? null
+          : (buttonWidth ?? (forAction ? buttonHeight : double.infinity)),
+      height: mini ? null : buttonHeight,
+      padding: mini ? const EdgeInsets.all(8) : null,
+      decoration: BoxDecoration(
+        border: buttonBorder ??
+            (borderWidth > 0
+                ? Border.all(
+                    color: Color.lerp(buttonColor, CustomColors.black, .06)!,
+                    width: borderWidth,
+                  )
+                : null),
+        borderRadius: BorderRadius.circular(
+          forAction ? buttonHeight : buttonBorderRadius,
         ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onTap,
-            onLongPress: onLongPress,
-            splashColor: _splashBaseColor.withOpacity(0.2),
-            highlightColor:
-                _splashBaseColor.withOpacity(isDark(buttonColor) ? 0.2 : 0.1),
-            child: Padding(
-              padding: padding ?? EdgeInsets.zero,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: forAction ? MainAxisSize.min : MainAxisSize.max,
-                children: [
-                  FitHorizontally(
-                    child: child ??
-                        Text(
-                          text!,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: buttonTextColor,
-                            fontWeight:
-                                bold ? FontWeight.w700 : FontWeight.w500,
-                            fontSize: buttonFontSize,
-                          ),
+        color: buttonColor,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          onLongPress: onLongPress,
+          splashColor: _splashBaseColor(buttonColor).withOpacity(0.2),
+          highlightColor: _splashBaseColor(buttonColor)
+              .withOpacity(isDark(buttonColor) ? 0.2 : 0.1),
+          child: Padding(
+            padding: padding ?? EdgeInsets.zero,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: forAction ? MainAxisSize.min : MainAxisSize.max,
+              children: [
+                FitHorizontally(
+                  child: child ??
+                      Text(
+                        text!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: buttonTextColor,
+                          fontWeight: bold ? FontWeight.w700 : FontWeight.w500,
+                          fontSize: buttonFontSize,
                         ),
-                  ),
-                ],
-              ),
+                      ),
+                ),
+              ],
             ),
           ),
         ),
-      );
-}
+      ),
+    );
--- lib/views/components/go_back_button.dart
+++ lib/views/components/go_back_button.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:get/get.dart';
 import 'package:parking_people_flutter/constants/custom_colors.dart';
 import 'package:parking_people_flutter/views/components/common/common_button.dart';
 
-class GoBackButton extends StatelessWidget {
-  const GoBackButton({Key? key}) : super(key: key);
+part 'go_back_button.g.dart';
 
-  @override
-  Widget build(BuildContext context) => CommonButton(
-        onTap: () {
-          navigator?.maybePop();
-        },
-        buttonColor: Colors.transparent,
-        child: const Icon(
-          Icons.chevron_left,
-          size: kToolbarHeight,
-          color: CustomColors.grey,
-        ),
-      );
-}
+@swidget
+Widget goBackButton() => CommonButton(
+      onTap: () {
+        navigator?.maybePop();
+      },
+      buttonColor: Colors.transparent,
+      child: const Icon(
+        Icons.chevron_left,
+        size: kToolbarHeight,
+        color: CustomColors.grey,
+      ),
+    );
