6b1688a0c8867e041694a37b7cf67e49748bf53b
Taejong Yoo <yut951121@gmail.com>
2022-12-02 08:58:30 +0900

    fix(ui): :bug: persistent intro dismissal state

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:logger/logger.dart';
 import 'package:parking_people_flutter/repository/api_client.dart';
 import 'package:parking_people_flutter/repository/rest_api.dart';
@@ -19,3 +22,26 @@ final ApiClient defaultApiClient =
     ParkingLotApi(Dio(), baseUrl: defaultApiBaseUrl);
 
 const defaultApiBaseUrl = "https://main.parkingpeople.app";
+
+late final FlutterSecureStorage storage;
+Future<bool> get splashDismissed async {
+  String splashDismissedString =
+      await storage.read(key: DataKeys.splashDismissed) ?? 'false';
+  return _boolSerializerMap[splashDismissedString] as bool;
+}
+
+const _boolSerializerMap = {
+  true: 'true',
+  false: 'false',
+  'true': true,
+  'false': false
+};
+set splashDismissed(FutureOr<bool> newSplashDismissed) {
+  storage.write(
+      key: DataKeys.splashDismissed,
+      value: _boolSerializerMap[newSplashDismissed] as String);
+}
+
+abstract class DataKeys {
+  static const splashDismissed = 'splashDismissed';
+}
