989a06f61746d6e3d3db88c5ae20cae3bfd4557b
Taejong Yoo <yut951121@gmail.com>
2022-12-05 15:50:52 +0900

    fix(l10n): :globe_with_meridians: Localized device app description

--- lib/parking_people_app.dart
+++ lib/parking_people_app.dart
@@ -6,6 +6,7 @@ import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 import 'package:parking_people_flutter/gen/colors.gen.dart';
 import 'package:parking_people_flutter/gen/fonts.gen.dart';
+import 'package:parking_people_flutter/translations.dart';
 import 'package:parking_people_flutter/utils/globals.dart';
 import 'package:parking_people_flutter/views/screens/splash_screen.dart';
 import 'package:parking_people_flutter/views/routes/routes.dart';
@@ -16,49 +17,50 @@ part 'parking_people_app.g.dart';
 Widget parkingPeopleApp(BuildContext context) {
   final bool shouldShowDebugUi = kDebugMode && !isDemoMode;
 
-  return AdaptiveTheme(
-    initial: AdaptiveThemeMode.system,
-    light: ThemeData(
-      colorScheme: ColorScheme.fromSwatch(
+  return I18n(
+    child: AdaptiveTheme(
+      initial: AdaptiveThemeMode.system,
+      light: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: ColorName.blue,
+            accentColor: ColorName.blueAccent,
+            brightness: Brightness.light),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: ColorName.black),
+        ),
+        fontFamily: FontFamily.spoqaHanSansNeo,
+        brightness: Brightness.light,
+      ),
+      dark: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
           primarySwatch: ColorName.blue,
           accentColor: ColorName.blueAccent,
-          brightness: Brightness.light),
-      textTheme: const TextTheme(
-        bodyText2: TextStyle(color: ColorName.black),
-      ),
-      fontFamily: FontFamily.spoqaHanSansNeo,
-      brightness: Brightness.light,
-    ),
-    dark: ThemeData(
-      colorScheme: ColorScheme.fromSwatch(
-        primarySwatch: ColorName.blue,
-        accentColor: ColorName.blueAccent,
+          brightness: Brightness.dark,
+        ),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: ColorName.lighterGrey),
+        ),
+        fontFamily: FontFamily.spoqaHanSansNeo,
         brightness: Brightness.dark,
       ),
-      textTheme: const TextTheme(
-        bodyText2: TextStyle(color: ColorName.lighterGrey),
+      builder: (light, dark) => MaterialApp(
+        navigatorKey: globalNavigatorKey,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('ko', 'KR'),
+          Locale('en', 'US'),
+        ],
+        onGenerateTitle: (context) => Strings.appName.i18n,
+        routes: Routes.routeMap,
+        home: const SplashScreen(),
+        theme: light,
+        darkTheme: dark,
+        debugShowCheckedModeBanner: shouldShowDebugUi,
       ),
-      fontFamily: FontFamily.spoqaHanSansNeo,
-      brightness: Brightness.dark,
-    ),
-    builder: (light, dark) => MaterialApp(
-      navigatorKey: globalNavigatorKey,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('ko', 'KR'),
-        Locale('en', 'US'),
-      ],
-      routes: Routes.routeMap,
-      home: I18n(
-        child: const SplashScreen(),
-      ),
-      theme: light,
-      darkTheme: dark,
-      debugShowCheckedModeBanner: shouldShowDebugUi,
     ),
   );
 }
